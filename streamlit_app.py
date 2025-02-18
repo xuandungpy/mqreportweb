2281b11fa82dc04aeb39ad33f99776edb0b0957f
David Dung <phamxuandungpy@gmail.com>
2025-02-18 08:34:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,12 @@ import streamlit as st
 import pandas as pd
 import requests
 
+# Gọi API để lấy dữ liệu
+response = requests.get('http://mqsoft.ddns.net:5000/get-data')
+data = response.json()
+
+# Chuyển dữ liệu thành DataFrame
+df = pd.DataFrame(data)
+
 st.title('MQ Report web')
+st.dataframe(df)
